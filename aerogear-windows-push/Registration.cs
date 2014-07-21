cc0de0988592996cd730bc01f6403a17c38a2f75
edewit <erikjan.dewit@gmail.com>
2014-07-21 16:32:50 +0100

    Initial version

---
+++ aerogear-windows-push/Registration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking.PushNotifications;
+using Windows.Security.ExchangeActiveSyncProvisioning;
+
+namespace AeroGear.Push
+{
+    public class Registration
+    {
+        public void Register(PushConfig pushConfig, MobileServiceClient mobileServiceClient)
+        {
+            RergisterAsync(pushConfig, mobileServiceClient).Wait();
+        }
+
+        static async Task RergisterAsync(PushConfig pushConfig, MobileServiceClient mobileServiceClient)
+        {
+
+            EasClientDeviceInformation deviceInformation = new EasClientDeviceInformation();
+            string os = deviceInformation.OperatingSystem;
+            string deviceType = deviceInformation.SystemProductName;
+
+            var channel = await PushNotificationChannelManager.CreatePushNotificationChannelForApplicationAsync();
+
+            Installation installation = new Installation() { alias = pushConfig.Alias, operatingSystem = os, osVersion = deviceType, categories = pushConfig.Categories, deviceToken = channel.Uri };
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = pushConfig.UnifiedPushUri;
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("Authorization", "Basic " + Convert.ToBase64String(UTF8Encoding.UTF8.GetBytes(pushConfig.VariantId + ":" + pushConfig.VariantSecret)));
+
+                HttpResponseMessage response = await client.PostAsJsonAsync<Installation>("rest/registry/device", installation);
+
+                response.EnsureSuccessStatusCode();
+
+                await mobileServiceClient.GetPush().RegisterNativeAsync(channel.Uri);
+            }
+        }
+    }
+}
---
+++ test/RegistrationTest.cs
@@ -0,0 +1,24 @@
+﻿using AeroGear.Push;
+using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace test
+{
+    [TestClass]
+    public class RegistrationTest
+    {
+        [TestMethod]
+        public void ShouldRegister()
+        {
+            //given
+            PushConfig config = new PushConfig() { UnifiedPushUri = new Uri("http://localhost:8080/ag-push/"), VariantId = "6db5c09e-6787-492a-9798-1bc5a2915603", VariantSecret = "4fec0b5f-608e-4f07-b761-a245f58134a2" };
+            Registration registration = new Registration();
+
+            //when
+            registration.Register(config);
+
+            //then
+
+        }
+    }
+}
