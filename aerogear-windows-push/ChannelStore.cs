5e892a0528ffdb25a133931a7ee98a8dabee01e4
edewit <erikjan.dewit@gmail.com>
2014-08-04 14:54:44 +0100

    changed to use FileIO

--- aerogear-windows-push/ChannelStore.cs
+++ aerogear-windows-push/ChannelStore.cs
@@ -3,61 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.Storage;
-using Windows.Storage.Streams;
 
 namespace AeroGear.Push
 {
+    using Windows.Storage;
     public class ChannelStore
     {
-        public void Save(string channel)
+        public async void Save(string channel)
         {
-            Task<StorageFile> openFile = OpenFile(CreationCollisionOption.ReplaceExisting);
-            openFile.Wait();
-            Task writeContent = WriteContent(openFile.Result, channel);
-            writeContent.Wait();
+            StorageFile file = await OpenFile(CreationCollisionOption.ReplaceExisting);
+            await FileIO.WriteTextAsync(file, channel);
         }
 
-        public string Read()
+        public async Task<string> Read()
         {
-            Task<StorageFile> openFile = OpenFile(CreationCollisionOption.OpenIfExists);
-            openFile.Wait();
-            Task<string> readContent = ReadContent(openFile.Result);
-            readContent.Wait();
-            return readContent.Result;
+            StorageFile file = await OpenFile(CreationCollisionOption.OpenIfExists);
+            return await FileIO.ReadTextAsync(file);
         }
 
         private async Task<StorageFile> OpenFile(CreationCollisionOption option)
         {
             StorageFolder localFolder = ApplicationData.Current.LocalFolder;
             return await localFolder.CreateFileAsync("channelStore", option);
-        }
-
-        private async Task WriteContent(StorageFile file, string contents)
-        {
-            using (IRandomAccessStream textStream = await file.OpenAsync(FileAccessMode.ReadWrite))
-            {
-                using (DataWriter textWriter = new DataWriter(textStream))
-                {
-                    textWriter.WriteString(contents);
-                    await textWriter.StoreAsync();
-                }
-            }
-        }
-
-        private async Task<string> ReadContent(StorageFile file)
-        {
-            string contents;
-            using (IRandomAccessStream textStream = await file.OpenReadAsync())
-            {
-                using (DataReader textReader = new DataReader(textStream))
-                {
-                    uint textLength = (uint)textStream.Size;
-                    await textReader.LoadAsync(textLength);
-                    contents = textReader.ReadString(textLength);
-                }
-            }
-            return contents;
         }
     }
 }
