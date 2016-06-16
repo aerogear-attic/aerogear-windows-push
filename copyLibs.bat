c243a3c5e745eaa42d52881261ad0146284eeddb
Hoyt Summers Pittman <secondsun@gmail.com>
2016-06-16 16:34:14 -0400

    Adding sources

--- copyLibs.bat
+++ copyLibs.bat
@@ -10,3 +10,7 @@ COPY /B lib\bin\Release\aerogear-windows-push.dll "Dist\lib\portable-win81+wp81+
 COPY /B lib\bin\Release\lib.pdb "Dist\lib\portable-win81+wp81+wpa81\"
 COPY /B lib\bin\Release\lib.dll "Dist\lib\portable-win81+wp81+wpa81\"
 
+IF not exist "Dist\src\aerogear-windows-push\" (md "Dist\src\aerogear-windows-push\")
+IF not exist "Dist\src\lib\" (md "Dist\src\lib\")
+XCOPY aerogear-windows-push\*.cs Dist\src\aerogear-windows-push /sy
+XCOPY lib\*.cs Dist\src\lib /sy
